4a1ffd494d7d343ceaa9a3791a871ff309bc7a67
berzeQ <artista.berzek@gmail.com>
2024-01-31 20:15:45 +0545

    some changes

--- client/src/app/projects/page.js
+++ client/src/app/projects/page.js
@@ -1,92 +1,111 @@
-'use client'
-import React, { useEffect,useState} from 'react'
-import CardView from '@/components/cardView/page'
-import Modal from '@/components/modal/page'
-import { Button, useDisclosure} from "@nextui-org/react";
-import DynamicForm from '@/components/dynamicForm/page';
-import { useSelector } from 'react-redux';
-import axios from 'axios';
-
+"use client";
+import React, { useEffect, useState } from "react";
+import CardView from "@/components/cardView/page";
+import Modal from "@/components/modal/page";
+import { Button, useDisclosure } from "@nextui-org/react";
+import DynamicForm from "@/components/dynamicForm/page";
+import { useSelector } from "react-redux";
+import axios from "axios";
 
 const page = () => {
-  const {userDetails} = useSelector(state=>state.user)
-  const [projectKey, setProjectKey] = useState('')
-  const {isOpen, onOpen, onOpenChange} = useDisclosure();
-  const [projectList, setProjectList] = useState([])
-  const [userList, setUserList] = useState([])
+  const { userDetails } = useSelector((state) => state.user);
+  const [projectKey, setProjectKey] = useState("");
+  const { isOpen, onOpen, onOpenChange } = useDisclosure();
+  const [projectList, setProjectList] = useState([]);
+  const [userList, setUserList] = useState([]);
 
-  const fetchProjectList =async ()=>{
-     const res = await fetch(`http://localhost:8080/projects`)
-     const data = await res.json()
-     setProjectList(data.projectList)
-  }
+  const fetchProjectList = async () => {
+    const res = await fetch(`http://localhost:8080/projects`);
+    const data = await res.json();
+    setProjectList(data.projectList);
+  };
 
-  const fetchUserList =async ()=>{
-    const res = await fetch(`http://localhost:8080/users`)
-    const data = await res.json()
-    setUserList(data.userList)
- }
+  const fetchUserList = async () => {
+    const res = await fetch(`http://localhost:8080/users`);
+    const data = await res.json();
+    setUserList(data.userList);
+  };
 
+  useEffect(() => {
+    fetchProjectList();
+    fetchUserList();
+  }, []);
 
-  useEffect(()=>{
-    fetchProjectList()
-    fetchUserList()
+  const onSave = async (inputFields) => {
+    inputFields.organization = userDetails.organization;
+    inputFields.projectKey = projectKey;
+    const { data } = await axios.post(
+      "http://localhost:8080/projects",
+      inputFields
+    );
+    onOpenChange();
+    fetchProjectList();
+  };
 
-  },[])
+  const generateKey = (inputProject, event) => {
+    if (event?.target.name === "projectName") {
+      if (!inputProject) return "";
+      let finalKey =
+        userDetails.organization.charAt(0).toUpperCase() +
+        inputProject
+          ?.split(" ")
+          ?.map((item) => item[0])
+          .join("")
+          .toUpperCase();
+      const projectKeys = projectList.map((item) => {
+        return item.projectKey;
+      });
 
-  const onSave = async(inputFields)=>{
-    inputFields.organization = userDetails.organization
-    inputFields.projectKey = projectKey
-    const {data} = await axios.post('http://localhost:8080/projects', inputFields )
-    onOpenChange()
-    fetchProjectList()
-  }
+      if (projectKeys.includes(finalKey)) {
+        finalKey =
+          finalKey +
+          inputProject[
+            Math.floor(Math.random() * inputProject.length)
+          ].toUpperCase();
+      }
 
-  const generateKey=( inputProject, event) => {
-    if(event?.target.name === 'projectName'){
-      if(!inputProject) return ''
-      let finalKey =   userDetails.organization.charAt(0).toUpperCase() + inputProject?.split(' ')?.map(item=>item[0]).join('').toUpperCase()
-          const projectKeys =projectList.map(item=>{
-            return item.projectKey
-        })
-        
-        if(projectKeys.includes(finalKey)){
-          finalKey= finalKey+ inputProject[Math.floor(Math.random()*inputProject.length)].toUpperCase()
-        }
-    
-      setProjectKey(finalKey)
-      return finalKey
+      setProjectKey(finalKey);
+      return finalKey;
     }
- }
+  };
   return (
     <div>
-      <Button onPress={onOpen} className='m-4' color="primary">
-      Add Projects
-    </Button>
-    <Modal
-     title="Add Projects"
-      isOpen={isOpen} 
-      onOpen={onOpen}
-      onOpenChange={onOpenChange}>
-        <DynamicForm 
-        buttonTitle= "Add"
-        onSave= {onSave}
-        projectKey={projectKey}
-        generateKey={generateKey}
-        formFields={[{label: 'projectName' ,placeholder:"Enter project Name"},
-                    {label: 'projectDescription',placeholder:"Enter project Description"},
-                     {label: 'members', placeholder:'Members', type:'Dropdown', data:userList},
-                     ]}/>
-        </Modal>
-     
+      <Button onPress={onOpen} className="m-4" color="primary">
+        Add Projects
+      </Button>
+      <Modal
+        title="Add Projects"
+        isOpen={isOpen}
+        onOpen={onOpen}
+        onOpenChange={onOpenChange}
+      >
+        <DynamicForm
+          buttonTitle="Add"
+          onSave={onSave}
+          projectKey={projectKey}
+          generateKey={generateKey}
+          formFields={[
+            { label: "projectName", placeholder: "Enter project Name" },
+            {
+              label: "projectDescription",
+              placeholder: "Enter project Description",
+            },
+            {
+              label: "members",
+              placeholder: "Members",
+              type: "Dropdown",
+              data: userList,
+            },
+          ]}
+        />
+      </Modal>
 
-     {projectList.map((item)=>{
+      {/* {projectList.map((item)=>{
       return       <CardView item={item}/>
-     })}
+     })} */}
+      <CardView projectList={projectList} />
+    </div>
+  );
+};
 
-      
-      </div>
-  )
-}
-
-export default page
+export default page;
--- client/src/components/cardView/page.js
+++ client/src/components/cardView/page.js
@@ -1,23 +1,170 @@
-'use client'
+"use client";
+import { React, useState, useMemo } from "react";
+import {
+  Table,
+  TableHeader,
+  TableColumn,
+  TableBody,
+  TableRow,
+  TableCell,
+  getKeyValue,
+  divider,
+  Pagination,
+} from "@nextui-org/react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTrash } from "@fortawesome/free-solid-svg-icons";
+import {
+  Dropdown,
+  DropdownTrigger,
+  DropdownMenu,
+  DropdownItem,
+  Button,
+} from "@nextui-org/react";
 
-import React from "react";
-import {Card, CardBody, CardFooter, Image} from "@nextui-org/react";
+const rows = [
+  {
+    key: "1",
+    name: "Tony Reichert",
+    role: "CEO",
+    status: "Active",
+  },
+  {
+    key: "2",
+    name: "Zoey Lang",
+    role: "Technical Lead",
+    status: "Paused",
+  },
+  {
+    key: "3",
+    name: "Jane Fisher",
+    role: "Senior Developer",
+    status: "Active",
+  },
+  {
+    key: "4",
+    name: "William Howard",
+    role: "Community Manager",
+    status: "Vacation",
+  },
+];
+const dropDown = [
+  {
+    key: "high",
+    label: "HIGH",
+  },
+  {
+    key: "low",
+    label: "LOW",
+  },
+  {
+    key: "normal",
+    label: "NORMAL",
+  },
+];
 
-const CardView =(props)=> {
+const columns = [
+  {
+    key: "projectKey",
+    label: "ID",
+  },
+
+  {
+    key: "projectName",
+    label: "PROJECT NAME",
+  },
+  {
+    key: "lead",
+    label: "ASSIGNEE",
+  },
+  {
+    key: "members",
+    label: "MEMBERS",
+  },
+  {
+    key: "status",
+    label: "STATUS",
+  },
+  {
+    key: "priority",
+    label: "PRIORITY",
+  },
+];
+
+export default function App(props) {
+  const [selectedKeys, setSelectedKeys] = useState([]);
+  console.log(selectedKeys);
+  const [page, setPage] = useState(1);
+  const rowsPerPage = 4;
+  const pages = Math.ceil(props.projectList.length / rowsPerPage);
+  const items = useMemo(() => {
+    const start = (page - 1) * rowsPerPage;
+    const end = start + rowsPerPage;
+
+    return props.projectList.slice(start, end);
+  }, [page, props.projectList]);
+
   return (
-    <div className="gap-2 grid grid-cols-2 sm:grid-cols-4 p-6">
-        <Card shadow="sm" isPressable onPress={() => console.log("item pressed")}>
-          <CardBody className="overflow-visible p-0">
-          </CardBody>
-          <CardFooter className="text-small justify-between">
-            <b>{props.item.projectName}</b>
-            <p className="text-default-500">{props.item.projectDescription}</p>
-          </CardFooter>
-        </Card>
-    </div>
+    <>
+      <Table
+        aria-label="Controlled table example with dynamic content"
+        selectionMode="multiple"
+        selectedKeys={selectedKeys}
+        onSelectionChange={setSelectedKeys}
+        bottomContent={
+          <div className="flex w-full justify-center">
+            <Pagination
+              isCompact
+              showControls
+              showShadow
+              color="secondary"
+              page={page}
+              total={pages}
+              onChange={(page) => setPage(page)}
+            />
+          </div>
+        }
+      >
+        <TableHeader columns={columns} className="py-5">
+          {(column) => (
+            <TableColumn key={column.key}>{column.label}</TableColumn>
+          )}
+        </TableHeader>
+        <TableBody items={items}>
+          {(item) => (
+            <TableRow key={item._id}>
+              {(columnKey) => {
+                let displayValue = item[columnKey];
+
+                return <TableCell>{displayValue}</TableCell>;
+              }}
+            </TableRow>
+          )}
+        </TableBody>
+      </Table>
+      {selectedKeys.size > 0 && (
+        <div className="flex justify-end items-center">
+          <button className="bg-white text-black text-5xl ">
+            <FontAwesomeIcon icon={faTrash} />{" "}
+          </button>
+
+          <Dropdown>
+            <DropdownTrigger>
+              <Button variant="bordered">PRIORITY </Button>
+            </DropdownTrigger>
+            <DropdownMenu aria-label="Dynamic Actions" items={dropDown}>
+              {(item) => (
+                <DropdownItem
+                  key={item.key}
+                  color={item.key === "high" ? "danger" : "default"}
+                  className={item.key === "high" ? "text-danger" : ""}
+                >
+                  {item.label}
+                </DropdownItem>
+              )}
+            </DropdownMenu>
+          </Dropdown>
+        </div>
+      )}
+    </>
   );
 }
-
-
-
-export default CardView
