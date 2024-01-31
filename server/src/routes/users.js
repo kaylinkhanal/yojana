23f8cff9934a994c4cf7d47ba8e39249a77dc038
Sudeep Rajbanshi <isudp.th@gmail.com>
2024-01-31 12:06:47 +0545

    change password feature performed

--- server/src/routes/users.js
+++ server/src/routes/users.js
@@ -1,7 +1,10 @@
 const express = require('express')
-const {registerNewUser,loginUser,getAllUsers} = require('../controllers/users')
+const {registerNewUser,loginUser,getAllUsers, changePassword} = require('../controllers/users')
 router = express.Router();
 router.post('/register',registerNewUser )
 router.post('/login',loginUser )
 router.get('/users',getAllUsers )
+
+//routes for change password
+router.post('/change-password', changePassword)
 module.exports = router
