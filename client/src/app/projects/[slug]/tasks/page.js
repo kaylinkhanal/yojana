efd0989631a115af6d1d89e3a2405389e0adf9f7
Kaylin Khanal <kaylinkhanal@Kaylins-MacBook-Pro.local>
2024-02-06 22:10:46 +0545

    fix for sortable js

--- client/src/app/projects/[slug]/tasks/page.js
+++ client/src/app/projects/[slug]/tasks/page.js
@@ -1,7 +1,8 @@
 'use client'
-import React, { useState } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import { ReactSortable } from "react-sortablejs";
-
+import AdminLayout from "@/components/adminLayout/page";
+import { Button } from "@nextui-org/react";
 
 const sortableOptions = {
   animation: 150,
@@ -12,86 +13,119 @@ const sortableOptions = {
   forceFallback: true
 };
 
-export default function App() {
-  const [spintsList, setSprintsList] = useState([
-    {
-      id: 1,
-      content: "Yojana 1.0",
-      parent_id: null,
-      type: "container",
-      tasks: [
-        {
-          id: 2,
-          content: "Create a login page",
-          width: 3,
-          type: "text",
-          parent_id: 1
-        },
-        {
-          id: 3,
-          content: "Create a register page",
-          width: 3,
-          type: "text",
-          parent_id: 1
-        }
-      ]
-    },
-    {
-      id: 4,
-      content: "Yojana 1.1",
-      parent_id: null,
-      type: "container",
-      tasks: [
-        {
-          id: 5,
-          content: "Change password feature",
-          width: 3,
-          parent_id: 2,
-          type: "container",
-          children: [
-            { id: 8, content: "Change password feature", width: 6, type: "text", parent_id: 5 },
-            { id: 9, content: "logout user", width: 6, type: "text", parent_id: 5 }
-          ]
-        },
-        {
-          id: 6,
-          content:  "logout user",
-          width: 2,
-          type: "text",
-          parent_id: 2
-        },
-        {
-          id: 7,
-          content: "esewa integrate",
-          width: 2,
-          type: "text",
-          parent_id: 2
-        }
-      ]
-    }
-  ]);
+const sprintStr =  {
+  id: 1,
+  content: "Yojana",
+  parent_id: null,
+  type: "container",
+  tasks: [
+   
+  ]
+}
 
+export default function App() {
+  const orgStr = {...sprintStr}
+  const inputRef = useRef(null)
+  const [activeForm, setActiveForm ]= useState(null)
+  const [spintsList, setSprintsList] = useState([
+    sprintStr
+  ]);
+  const handleActiveForm = (index) => {
+    setActiveForm(index);
+  };
+  const addSprint =()=>{
+    const existingSpintsList  = [...spintsList]
+    existingSpintsList.push({
+      id: 1,
+      content: "Yojana",
+      parent_id: null,
+      type: "container",
+      tasks: [
+
+      ]
+    })
+    setSprintsList(existingSpintsList)
+  }
+
+
+  useEffect(() => {
+    document.body.addEventListener("keydown", (e) => {
+      if (e.key === "Enter" && inputRef.current.value) {
+        const tempSpintsList = [...spintsList]
+        tempSpintsList[0].tasks.push({
+          content: inputRef.current.value,
+          id:  tempSpintsList[0].tasks.length+1
+        })
+        setSprintsList(tempSpintsList)
+      }
+    });
+  }, []);
   return (
-    <div>
-      <ReactSortable list={spintsList} setList={setSprintsList} {...sortableOptions}>
+    <AdminLayout>
+       <div className="flex flex-col items-end gap-4">
+        <Button onClick={addSprint}>Create Sprint</Button>
+      <ReactSortable 
+         className="w-full"
+      list={spintsList} setList={setSprintsList} {...sortableOptions}>
         {spintsList?.map((sprintItem, sprintId) => (
-            <div className="p-4 m-4 bg-red-500">
-              {sprintItem.content}
-              <ReactSortable  list={sprintItem.tasks} setList={(currentList)=>{
-                console.log("currentList", currentList)
+              <div
+              key={sprintItem.id}
+              className="p-4 m-4 bg-gray-200 flex flex-col items-start gap-1">
+              {sprintItem.content} {sprintId}
+              <ReactSortable  
+                 className="w-full"
+              list={sprintItem.tasks} setList={(currentList)=>{
                 setSprintsList((currentSprintList)=>{
-                  console.log("currentSprintList",currentSprintList)
-                  return currentSprintList
+                  const newList = [...currentSprintList]
+                  newList[sprintId].tasks =currentList
+                  return newList
                 })
-              }} >
-              {sprintItem.tasks.map((taskItem, taskId)=>{
-                return <div className="p-4 m-2 bg-blue-600">{taskItem.content}</div>
-              })}
+              }}
+              {...sortableOptions}
+              >
+              {sprintItem?.tasks.length>0 ? sprintItem.tasks.map((taskItem, taskId)=>{
+                return <div
+                key={taskItem.id}
+                className="p-4 m-2 bg-white">{taskItem.content}
+              {taskItem.content}</div>
+              }): (
+                <div className="w-full py-2 text-center border border-gray-500 border-dashed">
+                <p>
+                  Plan a sprint by dragging a issue or by creating new one
+                </p>
+              </div>
+              )}
+               <div className="w-full relative">
+                <Button
+                  onClick={() => handleActiveForm(sprintId)}
+                  className="w-full text-start hover:bg-gray-200 px-1 py-2 flex justify-start rounded-none"
+                >
+                  + Create issue
+                </Button>
+                <div
+                  className={`${
+                    activeForm === sprintId ? "flex" : "hidden"
+                  } absolute w-full h-full inset-0 bg-white border border-blue-600 items-center gap-3 px-2`}
+                >
+                  <select name="" id="" className="focus:outline-none">
+                    <option value="feature">Feature</option>
+                    <option value="bug">Bug</option>
+                  </select>
+                  <input
+                    ref={inputRef}
+                    type="text"
+                    placeholder="Enter issue title?"
+                    className="w-full focus:outline-none"
+                  />
+                </div>
+              </div>
               </ReactSortable>
+              
               </div>
         ))}
       </ReactSortable>
-    </div>
+      </div>
+    </AdminLayout>
   );
 }
 
